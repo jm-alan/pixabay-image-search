df99436c981327b3ac42f998eccd9188d2ea1f69
JM <j.maxwell.alan@gmail.com>
2023-02-02 01:57:30 -0500

    correctly forward basic queries

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 
 import { enableSearch, setQueryText } from '../../store/images/query/query-action-creators';
 
+import './search.css';
+
 export default function Search () {
   const dispatch = useDispatch();
 
@@ -15,6 +17,11 @@ export default function Search () {
     dispatch(enableSearch());
   };
 
+  const handleQueryUpdate = ({ target: { value } }) => {
+    if (value.length > 100) return;
+    updateQuery(value);
+  };
+
   return (
     <div id='searchbar-container'>
       <form
@@ -25,11 +32,13 @@ export default function Search () {
           id='searchbar-text'
           type='text'
           placeholder='What would you like to see more of in the world?'
-          onChange={({ target: { value } }) => updateQuery(value)}
+          value={query}
+          onChange={handleQueryUpdate}
         />
         <button
           id='searchbar-submit'
           type='submit'
+          disabled={!query}
         >
           <span className='material-symbols-outlined'>
             search
