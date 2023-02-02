463b7ee9db68b738aefdebd9e5afe24236bf85a9
JM <j.maxwell.alan@gmail.com>
2023-02-01 23:42:39 -0500

    create pixabay query forwarder

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 
+const pixabayQueryRouter = require('./pixabay-queries');
+
+router.use('/queries', pixabayQueryRouter);
+
 module.exports = router;
---
+++ backend/routes/api/pixabay-queries.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+const limitKeys = require('../../utils/middleware/limitPermittedKeys');
+
+router.post(
+  '/',
+  limitKeys(
+    'q',
+    'lang',
+    'id',
+    'image_type',
+    'orientation',
+    'category',
+    'min_width',
+    'min_height',
+    'colors',
+    'editors_choice',
+    'safesearch',
+    'order',
+    'page',
+    'per_page'
+  ),
+  asyncHandler(async (req, res) => {
+    const { body } = req;
+
+    let queryString = `https://pixabay.com/api/?key=${process.env.PIXABAY_KEY}`;
+
+    for (const key in body) {
+      queryString += `&${key}=${encodeURIComponent(body[key])}`;
+    }
+
+    const response = await fetch(queryString);
+    const rateLimit = response.headers.get('X-RateLimit-Limit');
+    const requestsRemaining = response.headers.get('X-RateLimit-Remaining');
+    const resetTimer = response.headers.get('X-RateLimit-Reset');
+
+    // If we receive specifically a 429, per the documentation, we've exceeded our
+    // rate limit
+    if (response.status === 429) {
+      return res.status(429).json({
+        errors: [
+          'API rate limit exceeded',
+          `Pixabay API will resume accepting requests in ${resetTimer} seconds`
+        ]
+      });
+    // If we get literally any other error from the Pixabay API, we likely aren't
+    // equipped to handle it, and should send our generic "oops" response on the
+    // frontend
+    } else if (response.status.between(400, 599)) {
+      console.error('Received bad response from Pixabay API');
+      console.error(`Status: ${response.status}`);
+      console.error(response.statusText);
+      return res.sendStatus(500);
+    }
+    // Otherwise, we're good to parse the response
+
+    const data = await response.json();
+
+    res.json({ data, rateLimit, requestsRemaining, resetTimer });
+  }));
+
+module.exports = router;
