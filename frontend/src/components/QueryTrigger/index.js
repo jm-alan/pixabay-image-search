2635bcbdbabb897be21c4f8e82ffaf049ab8dc53
JM <j.maxwell.alan@gmail.com>
2023-02-02 17:14:14 -0500

    remove logging from query trigger

--- frontend/src/components/QueryTrigger/index.js
+++ frontend/src/components/QueryTrigger/index.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getResults } from '../../store/images/display';
 import { disableSearch } from '../../store/images/query/query-action-creators';
-import { devlog } from '../../utils/logging';
 
 export default function QueryTrigger () {
   const dispatch = useDispatch();
@@ -11,7 +10,6 @@ export default function QueryTrigger () {
   const configuration = useSelector(state => state.images.query.configuration);
 
   useEffect(() => {
-    devlog('search enabled', enabled);
     if (enabled) {
       dispatch(disableSearch());
       dispatch(getResults(configuration));
