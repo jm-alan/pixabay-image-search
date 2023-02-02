fbc06d942ed769b4a9d08a85aa8d2aba0349ec2e
JM <j.maxwell.alan@gmail.com>
2023-02-02 17:31:24 -0500

    create & mount search config component

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Search from '../Search';
+import SearchConfig from '../SearchConfig';
 import SearchResults from '../SearchResults';
 
 import './home.css';
@@ -8,6 +9,7 @@ export default function Home () {
   return (
     <div id='home-container'>
       <Search />
+      <SearchConfig />
       <SearchResults />
     </div>
   );
