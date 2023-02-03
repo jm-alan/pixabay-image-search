05710798d3d9d40a93130e430fc8015e612416c5
JM <j.maxwell.alan@gmail.com>
2023-02-02 19:36:54 -0500

    fix dropdown height

--- frontend/src/components/SearchConfig/Dropdown.js
+++ frontend/src/components/SearchConfig/Dropdown.js
@@ -7,7 +7,12 @@ export default function Dropdown ({ entries, onChangeValue }) {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className={`custom-dropdown${expanded ? ' expanded' : ''}`}>
+    <div
+      className={`custom-dropdown${expanded ? ' expanded' : ''}`}
+      style={{
+        height: expanded ? `calc((((var(--dropdown-height) * 0.4) + 0.6rem) * ${entries.length}) + var(--dropdown-height))` : 'var(--dropdown-height)'
+      }}
+    >
       <div
         className='dropdown-selected'
         onClick={() => setExpanded(exp => !exp)}
